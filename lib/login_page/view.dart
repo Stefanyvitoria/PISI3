be39e3888f036d8cbab3cc426586e57b2f36a642
Stefany Izidio <stefanyvitoria9307@gmail.com>
2021-05-24 15:21:49 -0300

    - adjusting screen responsiveness. - updating button "login".

--- lib/login_page/view.dart
+++ lib/login_page/view.dart
@@ -9,24 +9,25 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: darkpurple,
-        child: ListView(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 0),
-              child: Container(
-                  height: 200,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/ANIMECOM.png'),
-                        fit: BoxFit.cover),
-                  )),
-            ),
-            Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Container(
+    double height = MediaQuery.of(context).size.height;
+
+    return Container(
+      height: height,
+      child: Scaffold(
+        body: Container(
+          color: darkpurple,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Container(
+                    height: height * 0.3,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/ANIMECOM.png'),
+                          fit: BoxFit.cover),
+                    )),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.6,
                   child: Column(
                     children: <Widget>[
                       Padding(
@@ -80,22 +81,29 @@ class _LoginPageState extends State<LoginPage> {
                       Padding(
                         padding:
                             const EdgeInsets.only(top: 20, left: 25, right: 25),
-                        child: FlatButton(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: new BorderRadius.circular(20.0)),
+                        child: TextButton(
+                          style: ButtonStyle(
+                            fixedSize: MaterialStateProperty.all<Size>(
+                                Size(MediaQuery.of(context).size.width, 50)),
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                              Color(0XFFDAE2E7),
+                            ),
+                            shape: MaterialStateProperty.all<OutlinedBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(20.0),
+                              ),
+                            ),
+                          ),
                           onPressed: () {},
                           child: Text(
                             'LOGIN',
                             style: quicksand(
                                 color: darkpurple,
-                                fontSize: 18,
+                                fontSize: 18.0,
                                 fontWeight: FontWeight.bold),
                           ),
-                          height: 50,
-                          minWidth: MediaQuery.of(context).size.width,
-                          color: Color(0XFFDAE2E7),
                         ),
-                      )
+                      ),
                     ],
                   ),
                   decoration: BoxDecoration(
@@ -103,36 +111,39 @@ class _LoginPageState extends State<LoginPage> {
                       borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(35),
                           topRight: Radius.circular(35))),
-                )),
-            Padding(
-              padding: EdgeInsets.only(top: 250),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "Don't have an account?",
-                    style: quicksand(
-                        color: linen,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.normal),
-                  ),
-                  TextButton(
-                      style: ButtonStyle(
-                        overlayColor:
-                            MaterialStateProperty.all(Colors.transparent),
-                      ),
-                      onPressed: () {},
-                      child: Text(
-                        'Sign Up',
+                ),
+                Container(
+                  height: height * 0.1,
+                  alignment: Alignment.bottomCenter,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Don't have an account?",
                         style: quicksand(
                             color: linen,
                             fontSize: 18.0,
-                            fontWeight: FontWeight.bold),
-                      ))
-                ],
-              ),
-            )
-          ],
+                            fontWeight: FontWeight.normal),
+                      ),
+                      TextButton(
+                          style: ButtonStyle(
+                            overlayColor:
+                                MaterialStateProperty.all(Colors.transparent),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            'Sign Up',
+                            style: quicksand(
+                                color: linen,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold),
+                          ))
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
