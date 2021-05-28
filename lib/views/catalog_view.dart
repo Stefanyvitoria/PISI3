871362ceb10c350847ebe0a09d919adab43cc1f4
Gabriel Dias da Silva <gdias9487@gmail.com>
2021-05-28 19:08:25 -0300

    - added some screens

--- lib/views/catalog_view.dart
+++ lib/views/catalog_view.dart
@@ -1,4 +1,5 @@
 import 'package:animecom/pre-sets.dart';
+import 'package:animecom/views/settings_view.dart';
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
@@ -164,6 +165,12 @@ class _CatalogState extends State<Catalog> {
                             color: darkpurple,
                           ),
                           ListTile(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SettingsPage()));
+                            },
                             leading: Icon(
                               Icons.settings,
                               color: darkpurple,
---
+++ lib/views/settings_view.dart
@@ -0,0 +1,168 @@
+import 'package:animecom/pre-sets.dart';
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: InkWell(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          child: Icon(
+            Icons.close,
+          ),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+        automaticallyImplyLeading: false,
+        title: Padding(
+          padding: EdgeInsets.only(right: 55.0),
+          child: Center(
+            child: Text(
+              'Settings',
+              style: quicksand(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: darkpurple),
+            ),
+          ),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: Container(
+        decoration: BoxDecoration(color: darkpurple),
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 15, top: 20),
+              child: Text(
+                'General',
+                style: quicksand(
+                    color: linen, fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Card(
+                  child: Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(
+                      Icons.language,
+                      color: darkpurple,
+                      size: 25,
+                    ),
+                    title: Text(
+                      'Language',
+                      style: quicksand(
+                          color: darkpurple,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Divider(
+                    height: 10.0,
+                    color: darkpurple,
+                  ),
+                  ListTile(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SettingsPage()));
+                    },
+                    leading: Icon(
+                      Icons.memory,
+                      color: darkpurple,
+                      size: 25,
+                    ),
+                    title: Text('Memory usage',
+                        style: quicksand(
+                            color: darkpurple,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  Divider(
+                    height: 10.0,
+                    color: darkpurple,
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.more_horiz,
+                      color: darkpurple,
+                      size: 25,
+                    ),
+                    title: Text('More',
+                        style: quicksand(
+                            color: darkpurple,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15, top: 20),
+              child: Text(
+                'Preferences',
+                style: quicksand(
+                    color: linen, fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Card(
+                  child: Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(
+                      Icons.brightness_6,
+                      color: darkpurple,
+                      size: 25,
+                    ),
+                    title: Text(
+                      'Dark/Light mode',
+                      style: quicksand(
+                          color: darkpurple,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Divider(
+                    height: 10.0,
+                    color: darkpurple,
+                  ),
+                  ListTile(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SettingsPage()));
+                    },
+                    leading: Icon(
+                      Icons.notifications,
+                      color: darkpurple,
+                      size: 25,
+                    ),
+                    title: Text('Notifications',
+                        style: quicksand(
+                            color: darkpurple,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              )),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
