82939c305799e0d5c98f311e810574e9dd764947
Stefany Izidio <stefanyvitoria9307@gmail.com>
2021-06-03 12:37:37 -0300

    add firestore model.

--- lib/controllers/sing_up_controller.dart
+++ lib/controllers/sing_up_controller.dart
@@ -1,11 +1,12 @@
+import 'package:animecom/models/firestore_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:animecom/models/user_model.dart';
 
 class SingUpController {
-  Future<DocumentReference> addUser({String email, String password}) async {
+  FirestoreModel firestoreModel = FirestoreModel();
+
+  Future<DocumentReference> addUser({String email, String password}) {
     User user = User('', '', email, '', '', '', password);
-    return await FirebaseFirestore.instance
-        .collection('Users')
-        .add(user.toJson());
+    return firestoreModel.add('Users', user);
   }
 }
